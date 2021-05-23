0ecab0fc1044bc948ab2c937c0c9c3c5bebe99fc
adityasunny1189 <adityapathak1189@gmail.com>
2021-05-23 15:24:51 +0530

    adding views templates and static files

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
+from django.urls import reverse
 
 months_and_challenges = {
 	"jan": "walk 20 min a day",
@@ -13,20 +14,32 @@ months_and_challenges = {
 	"sep": "Learn django for 20 mins",
 	"oct": "Learn Machine Learning",
 	"nov": "Learn Android Development",
-	"dec": "Learn django for 20 mins daily"
+	"dec": None
 }
 
 # Create your views here.
+def index(request):
+	months = list(months_and_challenges.keys())
+	return render(request, 'challenges/index.html', {
+		"months": months
+	})
+
+
 def monthly_challenge_num_wise(request, month):
 	months = list(months_and_challenges.keys())
 	if month > len(months):
-		return HttpResponseNotFound('Invalid Month')
-	redirect_month = months[month]
-	return HttpResponseRedirect('/challenges/' + redirect_month)
+		raise Http404()
+	redirect_month = months[month-1]
+	redirect_path = reverse('month-challenge', args=[redirect_month]) # /challenge/<month>
+	return HttpResponseRedirect(redirect_path)
+
 
 def monthly_challenge(request, month):
 	try:
 		challenge = months_and_challenges[month]
-		return HttpResponse(challenge)
+		return render(request, 'challenges/challenges.html', {
+				"month_name": month,
+				"month_challenge": challenge
+			})
 	except KeyError:
-		return HttpResponseNotFound('Invalid Month')
+		raise Http404()
